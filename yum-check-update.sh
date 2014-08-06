268d9b6769575bf47d0fd6af7608ff970ecf57b4
Roberto Gil <rgil@shub.es>
2014-08-06 07:28:38 +0200

    New  var

--- yum-check-update.sh
+++ yum-check-update.sh
@@ -2,7 +2,8 @@
 
 # Check and notify updates (yum check-update)
 
-USER="user@example.org"
+DEST="user@example.org"
+SENDER="root@example.org"
 SERVER="server.example.org"
 
 UPDATES=$(yum check-update -q)
@@ -10,5 +11,6 @@ UPDATES=$(yum check-update -q)
 if [ "$?" -eq "100" ]; then
     NUM_UPDATES=$(echo "$UPDATES" | grep -v ^$ | wc -l)
     echo "$UPDATES" | \
-    mail -s "yum: "$NUM_UPDATES" Updates Available (on $SERVER)" $USER
+    mail -r $SENDER \
+    -s "yum: "$NUM_UPDATES" Updates Available (on $SERVER)" $DEST
 fi
