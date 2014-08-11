796a1d4d17d3608eb5c6f594feb61b8e155fa96b
Roberto Gil <rgil@shub.es>
2014-08-11 13:39:56 +0200

    ldapsearch command check

--- ldap-searches.sh
+++ ldap-searches.sh
@@ -3,10 +3,17 @@
 # Common LDAP searches
 # Requires: ldapsearch (openldap-clients)
 
+if [ ! -f /usr/bin/ldapsearch ] ; then
+    echo "ldapsearch command not found, please install openldap-clients."
+    exit 1
+fi
+
 LDAP_USER="cn=user_example,cn=Users,dc=domain_example,dc=com"
 LDAP_PASS="PASSWORD"
 LDAP_SERVER="server.domain_example.com"
 LDAP_BASE="DC=domain_example,DC=com"
+
+OPTIONS="-w $LDAP_PASS -D $LDAP_USER -h $LDAP_SERVER -b $LDAP_BASE"
 
 usage() {
 cat << EOF
@@ -19,21 +26,19 @@ Options:
 EOF
 }
 
-OPTIONS="-w $LDAP_PASS -D $LDAP_USER -h $LDAP_SERVER -b $LDAP_BASE"
-
 if [ $# -gt "3" ] ; then
     usage
-# ./ldapsearch
 elif [ $# = 0 ] ; then
+    # ./ldapsearch
     ldapsearch $OPTIONS "(objectclass=*)"
-# ./ldapsearch login
 elif [ $# = 1 ] ; then
+    # ./ldapsearch login
     ldapsearch $OPTIONS "(sAMAccountName=$1)"
-# ./ldapsearch login attribute
 elif [ $# = 2 ] ; then
+    # ./ldapsearch login attribute    
     ldapsearch $OPTIONS "(sAMAccountName=$1)" $2
-# ./ldapsearch -d login attribute
 elif [ $# = 3 ] & [ $1 = "-d" ]; then
+    # ./ldapsearch -d login attribute
     ldapsearch $OPTIONS -LLL "(sAMAccountName=$2)" $3 | grep -v refldap | cut -d" " -f2 | base64 -di  
 fi
 
