397e185dba4aa792829cd653c3c9ce9c2c2fa47b
Roberto Gil <rgil@shub.es>
2014-08-27 14:46:34 +0200

    root and ssh checks

--- ssh-socks-tunnel.sh
+++ ssh-socks-tunnel.sh
@@ -2,9 +2,21 @@
 
 # SSH encrypted SOCKS tunnel
 # https://wiki.archlinux.org/index.php/Secure_Shell#Encrypted_SOCKS_tunnel
+# Requires: ssh (openssh-clients)
+# Version: 20140827
 
 PORT="example_port"
 USER="example_user"
 SERVER="example_server"
 
-ssh -v -TND $PORT $USER@$SERVER
+if [ ! ${EUID} == 0 ] ; then
+    echo "Root privileges are required."
+    exit 1
+fi
+
+if [ ! -f /usr/bin/ssh ] ; then
+    echo "ssh command not found, please install openssh-clients."
+    exit 1
+fi
+
+ssh -v -TND ${PORT} ${USER}@${SERVER}
