bae5f6bcae3fbda47795a91e426d5bb52ab310af
Roberto Gil <rgil@shub.es>
2014-08-28 09:11:09 +0200

    Support for old mailx

--- yum-check-update.sh
+++ yum-check-update.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# /etc/cron.daily/yum-check-update.sh
+#
 # Check and notify updates (yum check-update)
 # Requires: mailx
-# Version: 20140806
+# Version: 20140828
 
-DEST="user@example.org"
+USER="user@example.org"
 SENDER="root@example.org"
-SERVER="server.example.org"
 
 UPDATES=$(yum check-update -q)
 
 if [ "$?" -eq "100" ]; then
-    NUM_UPDATES=$(echo "$UPDATES" | grep -v ^$ | wc -l)
-    echo "$UPDATES" | \
-    mail -r $SENDER \
-    -s "yum: "$NUM_UPDATES" Updates Available (on $SERVER)" $DEST
+
+    NUM_UPDATES=$(echo "${UPDATES}" | grep -v ^$ | wc -l)
+
+    ### RHEL5 ###
+    # echo "${UPDATES}" | mail \
+    #    -s "yum: "${NUM_UPDATES}" Updates Available (on $(hostname))" ${USER} \
+    #    -- -f ${SENDER}
+
+    ### RHEL6+ ###
+    echo "${UPDATES}" | mail -r ${SENDER} \
+        -s "yum: "${NUM_UPDATES}" Updates Available (on $(hostname))" ${USER}
 fi
