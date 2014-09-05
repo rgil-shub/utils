5bda0478d16e0e094587f5abd5e6b71b43245f74
Roberto Gil <rgil@shub.es>
2014-09-05 10:57:16 +0200

    more comments

--- fujitsu-check-raid.sh
+++ fujitsu-check-raid.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Check Fujitsu RAID status
-# Version: 20140827
-# Requires: ServerView_RAID Manager RPM
-# FUJITSU Software Serverview Suite: 
-# http://support.ts.fujitsu.com/prim_supportcd/
+# Description: Check Fujitsu RAID status
+# Requires: 
+# * Install ServerView_RAID Manager Software
+# * Start Daemon for ServerView RAID Manager (aurad)
+# Version: 20140905
+
+# Software:
+# * FUJITSU Software Serverview Suite: 
+#   http://support.ts.fujitsu.com/prim_supportcd/
+# * Community Linux
+#   ftp://ftp.ts.fujitsu.com/community-linux
 
 export LD_LIBRARY_PATH="/opt/fujitsu/ServerViewSuite/RAIDManager/bin/"
 /opt/fujitsu/ServerViewSuite/RAIDManager/bin/amCLI -l all | grep "Status overall"
