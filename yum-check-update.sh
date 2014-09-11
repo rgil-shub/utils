8b7d82d02ad888c10aa1bc7b066259168a702949
Roberto Gil <rgil@shub.es>
2014-09-11 19:54:40 +0200

    remove two sorrounding quotes

--- yum-check-update.sh
+++ yum-check-update.sh
@@ -4,7 +4,7 @@
 #
 # Check and notify updates (yum check-update)
 # Requires: mailx
-# Version: 20140828
+# Version: 20140911
 
 USER="user@example.org"
 SENDER="root@example.org"
@@ -17,10 +17,10 @@ if [ "$?" -eq "100" ]; then
 
     ### RHEL5 ###
     # echo "${UPDATES}" | mail \
-    #    -s "yum: "${NUM_UPDATES}" Updates Available (on $(hostname))" ${USER} \
+    #    -s "yum: ${NUM_UPDATES} Updates Available (on $(hostname))" ${USER} \
     #    -- -f ${SENDER}
 
     ### RHEL6+ ###
     echo "${UPDATES}" | mail -r ${SENDER} \
-        -s "yum: "${NUM_UPDATES}" Updates Available (on $(hostname))" ${USER}
+        -s "yum: ${NUM_UPDATES} Updates Available (on $(hostname))" ${USER}
 fi
