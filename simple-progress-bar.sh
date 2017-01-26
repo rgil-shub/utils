1253ff2a066b6207d73bc487dbc4cca0efd7090d
Roberto Gil <rgil@shub.es>
2017-01-26 09:12:23 +0100

    simple progress bar

---
+++ simple-progress-bar.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+clear 
+echo 'Simple progress bar'
+echo -e '##                        (10%)\r\c'
+sleep 0.5
+echo -e '###                       (20%)\r\c'
+sleep 0.5
+echo -e '#####                     (33%)\r\c'
+sleep 0.5
+echo -e '#######                   (40%)\r\c'
+sleep 0.5
+echo -e '##########                (50%)\r\c'
+sleep 0.5
+echo -e '#############             (66%)\r\c'
+sleep 0.5
+echo -e '#####################     (99%)\r\c'
+sleep 0.5
+echo -e '#######################   (100%)\r\c'
+echo -e '\n'
+echo 'Complete!'
