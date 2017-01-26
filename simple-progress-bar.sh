7aaac23e65ebd81ffcf55d913bb0a89aff94df0f
Roberto Gil <rgil@shub.es>
2017-01-26 09:42:19 +0100

    Filips' changes

--- simple-progress-bar.sh
+++ simple-progress-bar.sh
@@ -1,21 +1,23 @@
 #! /bin/sh
 
-clear 
+clear
 echo 'Simple progress bar'
-echo -e '##                        (10%)\r\c'
+echo -e '[##                       ] (10%)\r\c'
 sleep 0.5
-echo -e '###                       (20%)\r\c'
+echo -e '[#####                    ] (20%)\r\c'
 sleep 0.5
-echo -e '#####                     (33%)\r\c'
+echo -e '[########                 ] (33%)\r\c'
 sleep 0.5
-echo -e '#######                   (40%)\r\c'
+echo -e '[##########               ] (40%)\r\c'
 sleep 0.5
-echo -e '##########                (50%)\r\c'
+echo -e '[#############            ] (50%)\r\c'
 sleep 0.5
-echo -e '#############             (66%)\r\c'
+echo -e '[#################        ] (66%)\r\c'
 sleep 0.5
-echo -e '#####################     (99%)\r\c'
+echo -e '[####################     ] (80%)\r\c'
 sleep 0.5
-echo -e '#######################   (100%)\r\c'
+echo -e '[######################## ] (99%)\r\c'
+sleep 0.5
+echo -e '[#########################] (100%)\r\c'
 echo -e '\n'
 echo 'Complete!'
