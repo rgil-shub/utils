2e45afa3b15f1154c66992882f1df4b3d2f9746e
Roberto Gil <rgil@shub.es>
2017-04-25 09:14:55 +0200

    obsolete apps

--- pacman-periodic-tasks.sh
+++ pacman-periodic-tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Description: Pacman periodic tasks
-# Version: 20140902
+# Version: 20170425
 
 # TASK 1: Optimize pacman database: 
 # # pacman-optimize
@@ -17,11 +17,15 @@
 #   community/9base 6-5               /opt/plan9/bin/bc
 #   community/plan9port 20140306-1    /usr/lib/plan9/bin/bc
 
-# TASK 3: Show and remove unused apps: 
+# TASK 3: Show and remove packages not in official repositories
+# $ pacman -Qm
+# # pacman -Rsn "$(pacman -Qm)"
+
+# TASK 4: Show and remove unused apps: 
 # $ pacman -Qdtq
 # # pacman -Rsn "$(pacman -Qdtq)"
 
-# TASK 4: 
+# TASK 5: 
 # > Deletes all the cached versions of each package except for 
 #   the most recent 3:
 # # paccache -r
@@ -45,10 +49,13 @@ echo "* Updating package file list..."
 pkgfile --update
 
 # task 3
+echo "* Obsolete apps..."
+
+# task 4
 echo "* Unused apps..."
 pacman -Rsn "$(pacman -Qdtq)"
 
-# task 4
+# task 5
 echo "* Removing cached packages (except most recent 3)..."
 paccache -r
 echo "* Removing all the cached versions of uninstalled packages..."
