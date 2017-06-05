79ae4460361c2a579365d682da62aac0e0a53c16
Roberto Gil <rgil@shub.es>
2017-06-05 07:39:19 +0200

    -blowfish, +compression

--- ssh-socks-tunnel.sh
+++ ssh-socks-tunnel.sh
@@ -3,13 +3,12 @@
 # SSH encrypted SOCKS tunnel
 # https://wiki.archlinux.org/index.php/Secure_Shell#Encrypted_SOCKS_tunnel
 # Requires: ssh (openssh-clients)
-# Version: 20150218
+# Version: 20170605
 
 PORT_SERVER="22"
 PORT_CLIENT="8080"
 USER="example_user"
 SERVER="example_server"
-CIPHER="blowfish"
 
 if [ ! ${EUID} == 0 ] ; then
     echo "Root privileges are required."
@@ -21,4 +20,4 @@ if [ ! -f /usr/bin/ssh ] ; then
     exit 1
 fi
 
-ssh -v -p ${PORT_SERVER} -c ${CIPHER} -TND ${PORT_CLIENT} ${USER}@${SERVER}
+ssh -v -C -p ${PORT_SERVER} -TND ${PORT_CLIENT} ${USER}@${SERVER}
